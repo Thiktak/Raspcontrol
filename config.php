6240fd5f97667c606f5d79832130d0ac37804907
Olivarès Georges <olivares.georges@gmail.com>
2013-05-08 13:47:18 +0200

    add auto HTTP_MOD_REWRITE detection

--- config.php
+++ config.php
@@ -6,7 +6,15 @@
  * Ensure you have done every other steps described on
  * https://github.com/Bioshox/Raspcontrol/wiki/Enable-URL-Rewriting#configure-your-web-server
  */
-$rewriting = false;
+
+
+// check if rewrite URL is enable
+if (function_exists('apache_get_modules')) {
+  $mod_rewrite = in_array('mod_rewrite', apache_get_modules()) && strtolower(getenv('HTTP_MOD_REWRITE')) == 'on';
+} else {
+  $mod_rewrite = strtolower(getenv('HTTP_MOD_REWRITE')) == 'on';
+}
+define('HTTP_MOD_REWRITE', $mod_rewrite);
 
 /*
  * Do NOT change the following lines
@@ -16,7 +24,7 @@ define('INDEX', './');
 define('LOGIN', 'login.php');
 define('FILE_PASS', '/etc/raspcontrol/database.aptmnt');
 
-if ($rewriting) {
+if ( HTTP_MOD_REWRITE ) {
   define('LOGOUT', './logout');
   define('DETAILS', './details');
 }
